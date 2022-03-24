beab41993a3ec76ebead35fc7f5016369d3520e2
Fedor Makareev <fmakareev@evrone.com>
2022-03-24 19:28:10 +0100

    удалил todo api

--- 14-02-2022/lesson-5-6/todo-api/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '1cdea6b28495b522de6fd55df89a1d68'),
-  },
-});
--- 14-02-2022/lesson-5-6/todo-api/src/api/todo/routes/todo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * todo router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::todo.todo');
